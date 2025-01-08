68b5d7ad92b4515cec5ce8df0c41140acc417d57
Abaul Dorian <abauldorian@gmail.com>
2025-01-08 19:55:51 +0100

    correction de l'exo 6 pour part 2

--- src/lib_poisson1D.c
+++ src/lib_poisson1D.c
@@ -4,133 +4,78 @@
 /* Poisson problem (Heat equation)            */
 /**********************************************/
 #include "lib_poisson1D.h"
-#include "lapacke.h"
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-// construit la matrice trigonale en format GB 
-void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv) {
-    int n = *la;
-    int k = *kv;
-    
-    for (int i = 0; i < (*lab) * n; i++) {
-        AB[i] = 0.0;
+void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv){
+  int ii, jj, kk;
+  for (jj=0;jj<(*la);jj++){
+    kk = jj*(*lab);
+    if (*kv>=0){
+      for (ii=0;ii< *kv;ii++){
+	AB[kk+ii]=0.0;
+      }
     }
-    
-    for (int j = 0; j < n; j++) {
-        AB[k + j*(*lab)] = 2.0;
-        
-        if (j > 0) {
-            AB[(k-1) + j*(*lab)] = -1.0;
-        }
-        
-        if (j < n-1) {
-            AB[(k+1) + j*(*lab)] = -1.0;
-        }
-    }
+    AB[kk+ *kv]=-1.0;
+    AB[kk+ *kv+1]=2.0;
+    AB[kk+ *kv+2]=-1.0;
+  }
+  AB[0]=0.0;
+  if (*kv == 1) {AB[1]=0;}
+  
+  AB[(*lab)*(*la)-1]=0.0;
 }
 
-// créer une matrice identité en bande 
-void set_GB_operator_colMajor_poisson1D_Id(double* AB, int *lab, int *la, int *kv) {
-    int n = *la;
-    for (int i = 0; i < n; ++i) {
-        AB[*lab + i] = 1.0;
-        *lab += *kv; 
+void set_GB_operator_colMajor_poisson1D_Id(double* AB, int *lab, int *la, int *kv){
+  int ii, jj, kk;
+  for (jj=0;jj<(*la);jj++){
+    kk = jj*(*lab);
+    if (*kv>=0){
+      for (ii=0;ii< *kv;ii++){
+	AB[kk+ii]=0.0;
+      }
     }
+    AB[kk+ *kv]=0.0;
+    AB[kk+ *kv+1]=1.0;
+    AB[kk+ *kv+2]=0.0;
+  }
+  AB[1]=0.0;
+  AB[(*lab)*(*la)-1]=0.0;
 }
 
-// définit le second membre du vecteur aux conditions limites de Dirichlet 
-void set_dense_RHS_DBC_1D(double* RHS, int* la, double* BC0, double* BC1) {
-    int n = *la;
-    
-    for (int i = 0; i < n; i++) {
-        RHS[i] = 0.0;
-    }
-    
-    RHS[0] = *BC0;
-    RHS[n-1] = *BC1;
+void set_dense_RHS_DBC_1D(double* RHS, int* la, double* BC0, double* BC1){
+  int jj;
+  RHS[0]= *BC0;
+  RHS[(*la)-1]= *BC1;
+  for (jj=1;jj<(*la)-1;jj++){
+    RHS[jj]=0.0;
+  }
+}  
+
+void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* BC0, double* BC1){
+  int jj;
+  double h, DELTA_T;
+  DELTA_T=(*BC1)-(*BC0);
+  for (jj=0;jj<(*la);jj++){
+    EX_SOL[jj] = (*BC0) + X[jj]*DELTA_T;
+  }
+}  
+
+void set_grid_points_1D(double* x, int* la){
+  int jj;
+  double h;
+  h=1.0/(1.0*((*la)+1));
+  for (jj=0;jj<(*la);jj++){
+    x[jj]=(jj+1)*h;
+  }
 }
 
-// donne la solution analytique
-void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* BC0, double* BC1) {
-    int n = *la;
-    double h = 1.0 / (n + 1);
-    
-    for (int i = 0; i < n; i++) {
-        double x = (i + 1) * h;
-        EX_SOL[i] = *BC0 * (1.0 - x) + *BC1 * x;
-    }
+double relative_forward_error(double* x, double* y, int* la){
+  return 0;
 }
 
-// définit les points de la grille
-void set_grid_points_1D(double* x, int* la) {
-    int n = *la; 
-    double h = 1.0 / (n + 1); 
-    
-    for (int i = 0; i < n; ++i) {
-        x[i] = (i + 1) * h; 
-    }
+int indexABCol(int i, int j, int *lab){
+  return j*(*lab)+i;
 }
 
-double relative_forward_error(double* x, double* y, int* la) {
-    double norm_diff = 0.0, norm_y = 0.0; 
-    
-    for(int i = 0; i < *la; i++) {
-        norm_diff += (x[i] - y[i]) * (x[i] - y[i]);
-        norm_y += y[i] * y[i];
-    }
-    
-    return sqrt(norm_diff) / sqrt(norm_y);
-}
-
-int indexABCol(int i, int j, int *lab) {
-    return 0;
-}
-
-int dgbtrftridiag(int *la, int *n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info) {
-    // Vérification des conditions pour une matrice tridiag
-    if (*lab != 4 || *kl != 1 || *ku != 1 || *la < *n) {
-        *info = -1;
-        return *info;
-    }
-    
-    *info = 0;
-    
-    // Factorisation LU pour matrice tridiagonale
-    for (int k = 0; k < *n - 1; k++) {
-        double diag = fabs(AB[indexABCol(k, k, lab)]);
-        double subdiag = fabs(AB[indexABCol(k + 1, k, lab)]);
-        
-        if (subdiag > diag) {
-            // Échange des lignes k et k+1
-            for (int j = k; j <= (k + 2 < *n ? k + 2 : *n - 1); j++) {
-                double temp = AB[indexABCol(k, j, lab)];
-                AB[indexABCol(k, j, lab)] = AB[indexABCol(k + 1, j, lab)];
-                AB[indexABCol(k + 1, j, lab)] = temp;
-            }
-            ipiv[k] = k + 2;  
-        } else {
-            ipiv[k] = k + 1;  
-        }
-        
-        // Vérification pivot nul
-        if (AB[indexABCol(k, k, lab)] == 0.0) {
-            *info = k + 1;
-            return *info;
-        }
-        
-        // Élimination de Gauss
-        double multiplier = AB[indexABCol(k + 1, k, lab)] / AB[indexABCol(k, k, lab)];
-        AB[indexABCol(k + 1, k, lab)] = multiplier;  // Stockage du multiplicateur dans L
-        AB[indexABCol(k + 1, k + 1, lab)] -= multiplier * AB[indexABCol(k, k + 1, lab)];
-    }
-    
-    // Dernier pivot
-    ipiv[*n - 1] = *n;
-    if (AB[indexABCol(*n - 1, *n - 1, lab)] == 0.0) {
-        *info = *n;
-    }
-    
-    return *info;
+int dgbtrftridiag(int *la, int*n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info){
+  return *info;
 }
--- src/tp_poisson1D_direct.c
+++ src/tp_poisson1D_direct.c
@@ -4,11 +4,6 @@
 /* to solve the Poisson 1D problem        */
 /******************************************/
 #include "lib_poisson1D.h"
-#include "lapacke.h"
-#include <cblas.h>
-#include <math.h>
-#include <time.h>
-#include <string.h>
 
 #define TRF 0
 #define TRI 1
@@ -28,10 +23,8 @@ int main(int argc,char *argv[])
   int IMPLEM = 0;
   double T0, T1;
   double *RHS, *EX_SOL, *X;
+  double **AAB;
   double *AB;
-  double *Y;  
-  clock_t start, end;
-  double time_used = 0.0;
 
   double relres;
 
@@ -58,10 +51,6 @@ int main(int argc,char *argv[])
   set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
   set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
   
-  // Save a copy of RHS before it gets modified by the solver
-  double* RHS_copy = (double*) malloc(sizeof(double)*la);
-  memcpy(RHS_copy, RHS, la*sizeof(double));
-  
   write_vec(RHS, &la, "RHS.dat");
   write_vec(EX_SOL, &la, "EX_SOL.dat");
   write_vec(X, &la, "X_grid.dat");
@@ -72,94 +61,43 @@ int main(int argc,char *argv[])
   lab=kv+kl+ku+1;
 
   AB = (double *) malloc(sizeof(double)*lab*la);
-  Y = (double *) malloc(sizeof(double)*la);  // Allocation de Y
 
   set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
   write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
-  
-  // Multiplication matrice-vecteur avec dgbmv
-  cblas_dgbmv(CblasColMajor, CblasNoTrans, la, la, kl, ku, 1.0, AB, lab, EX_SOL, 1, 0.0, Y, 1);
-  write_vec(Y, &la, "Mvec.dat");
 
   printf("Solution with LAPACK\n");
   ipiv = (int *) calloc(la, sizeof(int));
 
   /* LU Factorization */
   if (IMPLEM == TRF) {
-    clock_t start = clock();
     dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
-    clock_t end = clock();
-    double time_fact = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Time for factorization = %lf\n", time_fact);
-    
-    if (info == 0) {
-      start = clock();
-      dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
-      end = clock();
-      double time_solve = (double)(end - start) / CLOCKS_PER_SEC;
-      printf("Time for solve = %lf\n", time_solve);
-      printf("Total time = %lf\n", time_fact + time_solve);
-    }
   }
 
-  /* LU for tridiagonal matrix */
+  /* LU for tridiagonal matrix  (can replace dgbtrf_) */
   if (IMPLEM == TRI) {
-    clock_t start = clock();
     dgbtrftridiag(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
-    clock_t end = clock();
-    double time_fact = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Time for factorization = %lf\n", time_fact);
-    
-    if (info == 0) {
-      start = clock();
+  }
+
+  if (IMPLEM == TRI || IMPLEM == TRF){
+    /* Solution (Triangular) */
+    if (info==0){
       dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
-      end = clock();
-      double time_solve = (double)(end - start) / CLOCKS_PER_SEC;
-      printf("Time for solve = %lf\n", time_solve);
-      printf("Total time = %lf\n", time_fact + time_solve);
+      if (info!=0){printf("\n INFO DGBTRS = %d\n",info);}
+    }else{
+      printf("\n INFO = %d\n",info);
     }
   }
 
-  /* Direct solve */
+  /* It can also be solved with dgbsv */
   if (IMPLEM == SV) {
-    clock_t start = clock();
-    dgbsv_(&la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
-    clock_t end = clock();
-    double time_total = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Total time = %lf\n", time_total);
+    // TODO : use dgbsv
   }
 
   write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
   write_xy(RHS, X, &la, "SOL.dat");
 
-  /* Validation avec produit matrice-vecteur connu */
-  double* b_exact = (double*) malloc(sizeof(double)*la);
-  double* x_copy = (double*) malloc(sizeof(double)*la);
-  
-  // Sauvegarder une copie de la matrice AB avant qu'elle ne soit modifiée par dgbsv
-  double* AB_copy = (double*) malloc(sizeof(double)*lab*la);
-  memcpy(AB_copy, AB, sizeof(double)*lab*la);
-  
-  // Calculer b = A*x_exact
-  cblas_dgbmv(CblasColMajor, CblasNoTrans, la, la, kl, ku, 1.0, AB_copy, lab, EX_SOL, 1, 0.0, b_exact, 1);
-  write_vec(b_exact, &la, "b_exact.dat");
-  
-  // Copier b_exact car il sera modifié par dgbsv
-  memcpy(x_copy, b_exact, sizeof(double)*la);
-  
-  // Résoudre le système avec b_exact
-  dgbsv_(&la, &kl, &ku, &NRHS, AB_copy, &lab, ipiv, x_copy, &la, &info);
-  
-  // Comparer la solution obtenue avec EX_SOL
-  double validation_error = relative_forward_error(x_copy, EX_SOL, &la);
-  printf("\nValidation avec solution exacte: ||x - x_exact|| / ||x_exact|| = %e\n", validation_error);
-  
-  free(b_exact);
-  free(x_copy);
-  free(AB_copy);
-
   /* Relative forward error */
-  relres = relative_forward_error(RHS, EX_SOL, &la);  
+  relres = relative_forward_error(RHS, EX_SOL, &la);
   
   printf("\nThe relative forward error is relres = %e\n",relres);
 
@@ -167,6 +105,5 @@ int main(int argc,char *argv[])
   free(EX_SOL);
   free(X);
   free(AB);
-  free(Y);  
   printf("\n\n--------- End -----------\n");
 }
